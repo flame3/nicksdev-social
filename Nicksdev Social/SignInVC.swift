8d2ec5433697ec3ec06a9de9286e730963be7d57
nic <nicholaswillia5@gmail.com>
2016-10-24 19:04:40 -0400

    First Commit

---
+++ Nicksdev Social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nicksdev Social
+//
+//  Created by nic on 24/10/2016.
+//  Copyright © 2016 nicksdev. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Nicksdev Social/Pods-Nicksdev Social-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Nicksdev_SocialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Nicksdev_SocialVersionString[];
+
